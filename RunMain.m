a287f6e0ad677117a9546a9c70f0a0af111de01e
David Evans <dgevans42@gmail.com>
2012-10-03 13:56:51 -0400

    RunMain ready for inequality simulation

--- RunMain.m
+++ RunMain.m
@@ -3,7 +3,7 @@
  close all
 
 SetParaStruc
-Para.datapath=['Data/temp/Productivity/'];
+Para.datapath=['Data/temp/'];
 mkdir(Para.datapath)
 casename='Productivity';
 Para.StoreFileName=['c' casename '.mat'];
@@ -11,12 +11,34 @@ CoeffFileName=[Para.datapath Para.StoreFileName];
  
  %  --- SOLVE THE BELLMAN EQUATION --------------------------------------
  % test run 
-Para.Niter=250;
+Para.Niter=150;
 RGrid.RMin=2.5;
-RGrid.RMax=3.5;
+RGrid.RMax=3.2;
+Para.datapath=['Data/temp/Productivity/'];
+% LoadIndx=MainBellman(Para,RGrid);
+% NumIter=LoadIndx;
+% while NumIter < 200
+%     %InitData = load(CoeffFileName);
+% InitData=load([Para.datapath 'c_' num2str(LoadIndx) '.mat']);
+% RGrid.RMax=min(InitData.x_state(InitData.IndxUnSolved,2))*.95;
+% RGrid.RMin=2.5;
+% LoadIndx=MainBellman(Para,RGrid,InitData);
+% NumIter=NumIter+LoadIndx;
+% end
+
+% --- SOLVE THE BEllMAN FOR INEQUALITY SHOCKS -----------
+meantheta = mean([theta_1_low,theta_2_low]);
+Para.theta_1 = [theta_1_low;theta_1_low-0.05*meantheta];
+Para.theta_2 = [theta_2_low;theta_2_low+0.05*meantheta];
+Para.datapath=['Data/temp/Inequality/'];
+mkdir(Para.datapath)
+casename='Inequality';
+Para.StoreFileName=['c' casename '.mat'];
+CoeffFileName=[Para.datapath Para.StoreFileName];
+
 LoadIndx=MainBellman(Para,RGrid);
 NumIter=LoadIndx;
-while NumIter < 200
+while NumIter < 100
     %InitData = load(CoeffFileName);
 InitData=load([Para.datapath 'c_' num2str(LoadIndx) '.mat']);
 RGrid.RMax=min(InitData.x_state(InitData.IndxUnSolved,2))*.95;
@@ -27,25 +49,25 @@ end
 
 %-- Simulate the MODEL -------------------------------------------------
 NumSim=10000;
-sHist0=round(rand(NumSim,1))+1;
+sHist0=randi(2,NumSim,1);
 
 
-K=1;
+K=2;
 
 ex(1).casename='Productivity'; % benchmark calibrations high alpha1
-
+ex(2).casename='Inequality';
 
 
 for ctrb=1:K
-CoeffFileName=['Data/temp/Productivity/c' ex(ctrb).casename '.mat'];
+CoeffFileName=['Data/temp/' ex(ctrb).casename '/c' ex(ctrb).casename '.mat'];
 Sol=load(CoeffFileName);
 Param(ctrb)=Sol.Para;
 c10guess=1;
 c20guess=1/Param(ctrb).RMax;
 end
 
-for ctrb=1:K
-  CoeffFileName=['Data/temp/Productivity/c' ex(ctrb).casename '.mat'];
+parfor ctrb=1:K
+  CoeffFileName=['Data/temp/' ex(ctrb).casename '/c' ex(ctrb).casename '.mat'];
   
   [sHist(:,ctrb),u2btildHist(:,ctrb),RHist(:,ctrb),TauHist(:,ctrb),YHist(:,ctrb),TransHist(:,ctrb),...
           btildHist(:,ctrb),c1Hist(:,ctrb),c2Hist(:,ctrb),l1Hist(:,ctrb),l2Hist(:,ctrb),IntHist(:,ctrb),...
@@ -54,8 +76,9 @@ for ctrb=1:K
           LaborTaxAgent1DiffHist(:,ctrb),LaborTaxAgent2DiffHist(:,ctrb),DebtDiffHist(:,ctrb),...
           GiniCoeffHist(:,ctrb),theta_1Hist(:,ctrb),theta_2Hist(:,ctrb)]=RunSimulations(CoeffFileName,NumSim,Para,sHist0);
 end
+Para.datapath=['Data/temp/'];
 
-save( [Para.datapath 'SimDataParallelProductivity.mat'],'sHist',...
+savPara.datapath=['Data/temp/'];e( [Para.datapath 'SimDataParallelProductivity.mat'],'sHist',...
        'u2btildHist','RHist','TauHist','YHist','TransHist',...
        'btildHist','c1Hist','c2Hist','l1Hist','l2Hist','Para','IntHist',...
        'AfterTaxWageIncome_Agent1Hist','AfterTaxWageIncome_Agent2Hist',...
@@ -67,19 +90,19 @@ save( [Para.datapath 'SimDataParallelProductivity.mat'],'sHist',...
    
    
 %  % -- PLOT DIAGNOSTICS -----------------------------------------
-close all
-clear all
-clc
-SimTitle={'Productivity'};
-SimDataPath= 'Data/temp/Productivity/SimDataParallelProductivity.mat';
-SimPlotPath='Graphs/Productivity/';
-mkdir(SimPlotPath)
-SimTexPath='Tex/Productivity/';
-mkdir(SimTexPath)
+% close all
+% clear all
+% clc
+% SimTitle={'Productivity'};
+% SimDataPath= 'Data/temp/Productivity/SimDataParallelProductivity.mat';
+% SimPlotPath='Graphs/Productivity/';
+% mkdir(SimPlotPath)
+% SimTexPath='Tex/Productivity/';
+% mkdir(SimTexPath)
 % PlotParallelSimulationsCommonShocks(SimDataPath,SimTexPath,SimPlotPath,SimTitle)
 % 
 % 
-%  Para.datapath=['Data/temp/Productivity/'];
+%  Para.datapath=['Data/temp/Inequality/'];
 %  Para.StoreFileName=['c_25.mat'];
 %  
 %  GetPlotsForFinalSolution(Para)
