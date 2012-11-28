4e65e776e6017d8ab61b185fec8e5bc2ea076563
David Evans <dgevans42@gmail.com>
2012-11-28 13:24:13 -0500

    Replaced R with Rprime

--- computeL.m
+++ computeL.m
@@ -1,16 +1,16 @@
-function [l1 gradl1 l2 gradl2] = computeL(c1,gradc1,c2,gradc2,R,gradR,...
+function [l1 gradl1 l2 gradl2] = computeL(c1,gradc1,c2,gradc2,Rprime,gradRprime,...
                                             theta_1,theta_2,g,n1,n2)
     
     %Get g in right format for computations
     g = kron(ones(3,1),g');
     %Compute l2 first
-    l2 = ( n1*c1+n2*c2+g+n1*theta_2*R-n1*theta_1  )./(theta_2*(n2+R*n1));
+    l2 = ( n1*c1+n2*c2+g+n1*theta_2*Rprime-n1*theta_1  )./(theta_2*(n2+Rprime*n1));
     %now gradl2
-    gradl2 = n1*gradR./(n2+n1*R) - n1*gradR.*l2./(n2+n1*R)...
-             +n1*gradc1./(theta_2*(n2+n1*R)) +n2*gradc2./(theta_2*(n2+n1*R));
+    gradl2 = n1*gradRprime./(n2+n1*Rprime) - n1*gradRprime.*l2./(n2+n1*Rprime)...
+             +n1*gradc1./(theta_2*(n2+n1*Rprime)) +n2*gradc2./(theta_2*(n2+n1*Rprime));
     %now l1
-    l1 = 1 - (1-l2).*R*theta_2/theta_1;
-    gradl1 = gradl2.*R*theta_2/theta_1 - (1-l2).*gradR*theta_2/theta_1;
+    l1 = 1 - (1-l2).*Rprime*theta_2/theta_1;
+    gradl1 = gradl2.*Rprime*theta_2/theta_1 - (1-l2).*gradRprime*theta_2/theta_1;
          
          
 end
