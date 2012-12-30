bc700f7db0d36ad68348901e5373d7c2870ae205
Chase <cc7768@Laptop.(none)>
2012-12-30 00:57:38 -0700

    Finished function SS_Residuals

--- gspy/steady/steady_state.py
+++ gspy/steady/steady_state.py
@@ -160,4 +160,25 @@ def ss_residuals(X, Params):
     + theta_2 * phi * (1.0 - l2) \
     + rho * uc1
 
+    #Appeared to be an alternate method to build res, didn't translate.  Put here in case 
+    #we need it in the future
+
+    ##% res(1:2) =x*uc2/(beta*Euc2)-( psi*uc2.*(c2-c1)+x+(1-psi)*( R*l1./(1-l1) - l2./(1-l2) ) );
+    ##% 
+    ##% res(3:4) = theta_1*(1-l1)-theta_2*(1-l2)*R;
+    ##% 
+    ##% res(5:6) = theta_1*l1+theta_2*l2-c1-c2-g;
+    ##% 
+    ##% res(7:8) = psi*alpha_1*P.*uc1-psi*mu*P.*uc2-xi;
+    ##% 
+    ##% res(9:10) = alpha_2*psi*P.*uc2-mu*P.*( psi*(sigma*c1-(sigma-1)*c2)./(c2.^(sigma+1)) ...
+    ##%     -sigma*x*(Euc2-P.*uc2)./(c2.^(sigma+1)*Euc2^2) )+sigma*x*mu*Palt.*P.*uc2Alt./(beta*c2.^(sigma+1).*Euc2^2) ;
+    ##% 
+    ##% res(11:12) = theta_1*(xi+phi)-alpha_1*(1-psi)*P./(1-l1) -(1-psi)*R*mu*P./( (1-l1).^2 );
+    ##% 
+    ##% res(13:14) = theta_2*(xi-R*phi) - alpha_2*(1-psi)*P./(1-l2) + mu.*P./( (1-l2).^2 );
+    ##% 
+    ##% res(15:16) = beta*P*lambda*Euc1 - mu*(1-psi).*P.*l1./(1-l1)...
+    ##%     -lambda*P.*uc1+phi.*(1-l2);
+
     return res
