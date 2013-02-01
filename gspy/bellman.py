Fixed derivative method in interp.py and working on checking gradient

gspy/bellman.py
@@ -196,9 +196,10 @@ def init_coef(params, info_dict):
             c0[_s, :] = c0[_s - 1, :]
             V0[_s, :] = V0[_s - 1, :]
             xInit_0[_s, :, :] = xInit_0[_s - 1, :, :]
+            splines = [spline, spline]
 
-    domain_1 = np.column_stack((_domain, np.ones((n_size, 1))))
-    domain_2 = np.column_stack((_domain, np.ones((n_size, 1)) * 2))
+    domain_1 = np.column_stack((_domain, np.ones((_domain.shape[0], 1))))
+    domain_2 = np.column_stack((_domain, np.ones((_domain.shape[0], 1)) * 2))
     domain = np.concatenate((domain_1, domain_2), axis=0)
 
     c = c0
@@ -221,9 +222,11 @@ def init_coef(params, info_dict):
     # savemat('/users/spencerlyon2/documents/research/golosov-sargent/' + \
     #           'gspy/data/debugging/init_coefs.mat', data)
 
-    # NOTE: All objects in data are within 1e-13 of corresponding MatLab objs
+    # NOTE: All objects in data are within 1e-13 of corresponding MatLab objs.
+    #       This was true on 1-30-13 after adding our spline. Obviously
+    #       the coefficients aren't the same.
 
-    return [domain, c, policy_rules_store]
+    return [domain, c, policy_rules_store, splines]
 
 
 def main(params):
@@ -242,10 +245,8 @@ def main(params):
 
     #INITIALIZE THE COEFF
     print('Msg: Initializing the Value Function...')
-    [domain, c, policy_rules_store] = init_coef(params.copy(), info_dict)
+    [domain, c, policy_rules_store, splines] = init_coef(params.copy(), info_dict)
     print('Msg: ... Completed')
-
-    ctest = c  # NOTE: THis is just so the object c is available in pdb
 
     #Iterate on the Value Function
     #This block iterates on the bellman equation
@@ -283,7 +284,8 @@ def main(params):
             xInit = policy_rules_store[ctr, :]
 
             #Inner Optimization
-            policyrules, v_new = check_grad(x, R, s, c, info_dict, xInit, params)
+            policyrules, v_new = check_grad(x, R, s, c, info_dict, xInit, params,
+                                            splines)
             vnew[ctr, 0] = v_new
 
             #Update Policy rules

gspy/inner_opt.py
@@ -6,6 +6,7 @@ Python code that matches MatLab code from  ./InnerOptimizationCode
 from __future__ import division
 import numpy as np
 import numexpr as ne
+from scipy.optimize import root
 from set_params import DotDict
 from compeconpy import funeval
 
@@ -175,7 +176,7 @@ def uAlt(c, l, psi, sigma):
     return u
 
 
-def check_grad(xx, rr, ss, c, vv, z_init, params):
+def check_grad(xx, rr, ss, c, vv, z_init, params, spline):
     """
     Mimics the file ./InnerOptimizationCode/CheckGradNag.m
 
@@ -199,6 +200,9 @@ def check_grad(xx, rr, ss, c, vv, z_init, params):
     params:
         Dictionary containing parameters for the mode
 
+    spline: CubicSpline2d
+        The spline object created in bellman.py:init_coefs()
+
     """
     z_init = z_init[:3]
     params.theta = [params.theta_1, params.theta_2]
@@ -206,7 +210,7 @@ def check_grad(xx, rr, ss, c, vv, z_init, params):
     par = params
     x = xx
     r = rr
-    v_coef = [c[0, :].T, c[1, :].T]
+    v_coef = [c[:, :, 0], c[:, :, 1]]  # Put coefficients in list.
     v = vv
     _s = ss
     xLL = par.xLL
@@ -225,8 +229,11 @@ def check_grad(xx, rr, ss, c, vv, z_init, params):
     globs.x = x
     globs.params = par
     globs._s = _s
+    globs.spline = spline
 
-    bel_obj_uncond_grad(0, z_init, 0, 0, globs)
+    # TODO: Just stopped here and haven't seen if root can solve it yet.
+    z = root(bel_obj_uncond_grad, z_init, args=(globs))
+
 
     # NOTE: As of scipy version 0.11.0 scipy.optimize.root can be used to
     #       call the same routine used by the function c05qb within NAG.
@@ -234,8 +241,10 @@ def check_grad(xx, rr, ss, c, vv, z_init, params):
     #       hyrbid powell method.
 
 
-def bel_obj_uncond_grad(n, z, user, iflag, globs):
+def bel_obj_uncond_grad(z, globs):
     """
+    TODO: Fix this docstring to be in numpydoc format
+
     Mimics ./InnerOptimizationCode/BelObjectiveUncondGradNAGBGP.m
 
     Computes the gradient of the bellman equation objective with
@@ -243,8 +252,6 @@ def bel_obj_uncond_grad(n, z, user, iflag, globs):
     of the variables using their respective gradients.
 
     z is the vector containing these three variables
-
-    TODO: Fix this docstring to be in numpydoc format
 
     Notes
     -----
@@ -255,9 +262,6 @@ def bel_obj_uncond_grad(n, z, user, iflag, globs):
     struct containig all the relevent parameters.  We return the
     gradient grad.  user and iflag our variables that nag requires
     but we don't use.
-
-    the parameter n is just the number of equations. This can simply
-    be inferred from z.size.
 
     The user and iflag parameters are simply NAG things that aren't
     used
@@ -292,40 +296,53 @@ def bel_obj_uncond_grad(n, z, user, iflag, globs):
         c1_2 = z[1]
         c2_1 = z[2]
 
-    # NOTE: If there is an error in passing arguments to these other
-    #       functions it is because I compared the calls in
-    #       BelObjectiveUncondGradNAGBGP.m to those in SteadyStateResiduals.m
-    #       and the only difference was passing the x as the last arg
-    #       of compute_X_prime instead of u2btild.
+        # NOTE: If there is an error in passing arguments to these other
+        #       functions it is because I compared the calls in
+        #       BelObjectiveUncondGradNAGBGP.m to those in SteadyStateResiduals.m
+        #       and the only difference was passing the x as the last arg
+        #       of compute_X_prime instead of u2btild.
 
-    c1, c2, gradc1, gradc2 = computeC2_2(c1_1, c1_2, c2_1, r, _s, P, sigma)
-    r_prime, gradRprime = computeR(c1, c2, gradc1, gradc2, sigma)
-    l1, gradl1, l2, gradl2 = computeL(c1, gradc1, c2, gradc2, r_prime,
-                                 gradRprime, th_1, th_2, g, n1, n2)
-    xprime, gradxprime = compute_X_prime(c1, gradc1, c2, gradc2, r_prime,
-                                        gradRprime, l1, gradl1, l2, gradl2,
-                                            P, sigma, psi, beta, _s, x)
+        c1, c2, gradc1, gradc2 = computeC2_2(c1_1, c1_2, c2_1, r, _s, P, sigma)
+        r_prime, gradRprime = computeR(c1, c2, gradc1, gradc2, sigma)
+        l1, gradl1, l2, gradl2 = computeL(c1, gradc1, c2, gradc2, r_prime,
+                                     gradRprime, th_1, th_2, g, n1, n2)
+        xprime, gradxprime = compute_X_prime(c1, gradc1, c2, gradc2, r_prime,
+                                            gradRprime, l1, gradl1, l2, gradl2,
+                                                P, sigma, psi, beta, _s, x)
 
-    print 'nothing'
+        V_x = np.zeros((3, 2))
+        V_R = np.zeros((3, 2))
 
-    V_x = np.zeros((3, 2))
-    V_R = np.zeros((3, 2))
+        sp1 = globs.spline[0]
+        sp2 = globs.spline[1]
 
-    V_x[:, 0] = funeval(Vcoef[0], V[0],
-                        np.array([xprime[0, 0], r_prime[0, 0]]),
-                        np.array([1, 0]))
-    V_x[:, 1] = funeval(Vcoef[1], V[1],
-                        np.array([xprime[0, 1], r_prime[0, 1]]),
-                        np.array([1, 0]))
-    V_R[:, 0] = funeval(Vcoef[0], V[0],
-                        np.array([xprime[0, 0], r_prime[0, 0]]),
-                        np.array([0, 1]))
-    V_R[:, 1] = funeval(Vcoef[1], V[1],
-                        np.array([xprime[0, 1], r_prime[0, 1]]),
-                        np.array([0, 1]))
+        g00 = sp1.gradient([xprime[0, 0], r_prime[0, 0]])
+        g01 = sp2.gradient([xprime[0, 1], r_prime[0, 1]])
 
-    print 'nothing'
+        V_x[:, 0] = g00[0]
+        V_x[:, 1] = g01[0]
+        V_R[:, 0] = g00[1]
+        V_R[:, 1] = g01[1]
 
+        # compute the gradient of the objective function with respect to the
+        # choice variable z = [c_1(1) c_1(2) c_2(1)] using the gradients
+        # computed above.  Note gradV is a 3x2 matrix as we have computed the
+        # gradient for each of the two possible states
+        gradV = alpha[0] * psi * c1 ** (-sigma) * gradc1 \
+                + alpha[1] * psi * c2 ** (-sigma) * gradc2 \
+                - alpha[0] * (1 - psi) / (1 - l1) * gradl1 \
+                - alpha[1] * (1 - psi) / (1 - l2) * gradl2 \
+                + beta * (V_x * gradxprime + V_R * gradRprime)
 
-    # TODO: Stopping on line 80 of the MatLab. Need to write funeval from
-    #       compecon
+        grad = gradV.dot(P[_s, :])
+
+        if l1.max() > 1 or l2.max > 1:
+            grad = np.abs(z) + 100
+
+        if grad.imag.any():
+            grad = np.abs(grad) + 100
+
+    else:
+        grad = np.abs(z) + 100
+
+    return grad

gspy/interp.py
@@ -17,6 +17,7 @@ from interpolate.cubicspline1d import *
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import numdifftools as nd
+from scipy.misc import derivative as der
 
 __all__ = ['CubicSpline2d']
 
@@ -230,28 +231,33 @@ class CubicSpline2d(object):
         -----
         Uses numdifftools.Gradient.
         """
-        grad = nd.Gradient(self.eval)
-        return grad(point)
+        g1 = lambda x: self.eval([x, point[1]])
+        g2 = lambda r: self.eval([point[0], r])
+        grad = np.zeros_like(point)
+        grad[0] = der(g1, point[0], dx=0.0001, order=9)
+        grad[1] = der(g2, point[1], dx=0.0001, order=9)
+        return grad
+
 
 if __name__ == '__main__':
-    cs2d = CubicSpline2d(0, 0, 4, 4, 60, 60, 0, 0)
+    cs2d = CubicSpline2d(0, 0, 4, 4, 19, 19, 0, 0)
     Y, X = np.meshgrid(cs2d.ygrid, cs2d.xgrid)
     Z = np.sin(X) - np.cos(Y ** 2)
     cs2d.coefs(Z)
-    xtest = np.r_[0.0:4.0:100j]
-    ytest = np.r_[0.0:4.0:100j]
-    ztest = cs2d.eval(np.row_stack([xtest, ytest]))
+    # xtest = np.r_[0.0:4.0:100j]
+    # ytest = np.r_[0.0:4.0:100j]
+    # ztest = cs2d.eval(np.row_stack([xtest, ytest]))
 
-    # Build grid and get exact solution
-    yy, xx = np.meshgrid(ytest, xtest)
-    zz = np.sin(xx) - np.cos(yy ** 2)
+    # # Build grid and get exact solution
+    # yy, xx = np.meshgrid(ytest, xtest)
+    # zz = np.sin(xx) - np.cos(yy ** 2)
 
-    # Compute errors
-    max_abs_err = np.abs(ztest - zz).max()
-    mean_abs_err = np.abs(ztest - zz).mean()
+    # # Compute errors
+    # max_abs_err = np.abs(ztest - zz).max()
+    # mean_abs_err = np.abs(ztest - zz).mean()
 
-    print 'Max absolute error is ', max_abs_err
-    print 'Mean absolute error is ', mean_abs_err
+    # print 'Max absolute error is ', max_abs_err
+    # print 'Mean absolute error is ', mean_abs_err
 
     fun_real_grad = lambda x, y: np.array([np.cos(x), 2 * y * np.sin(y ** 2)])
 
@@ -259,3 +265,4 @@ if __name__ == '__main__':
     est_grad = cs2d.gradient([0.22, 2.45])
     grad_error = real_grad - est_grad
     print 'Error in gradient evaluation is: ', grad_error
+