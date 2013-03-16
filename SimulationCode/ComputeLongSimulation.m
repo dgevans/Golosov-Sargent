b7373357e89278c7631af1fd922cc69ba75cc059
David Evans <dgevans42@gmail.com>
2013-03-16 11:40:42 -0400

    Setup Compute Long Simulations

--- SimulationCode/ComputeLongSimulation.m
+++ SimulationCode/ComputeLongSimulation.m
@@ -2,7 +2,7 @@
  clear all
  close all
 
-load ('~/Golosov-Sargent/Data/temp/cproductivity.mat');
+load ('~/projects/Golosov-Sargent/Data/temp/cproductivity.mat');
 g=Para.g;
 n1=Para.n1;
 n2=Para.n2;
@@ -30,24 +30,26 @@ c10 = x(1);
 c20 = x(2);
 R0=(c10/c20)^(sigma);
 TotalResources=(c10*n1+c20*n2+g(s_));
-DenL2=theta_2*R0*n1+theta_2*n2;
-l20=(TotalResources-theta_1*n1+ theta_2*n1*R0)/(DenL2);
-l10= 1-(1-l20)*theta_2/theta_1*R0;
+DenL2=theta_2(s_)*R0*n1+theta_2(s_)*n2;
+l20=(TotalResources-theta_1(s_)*n1+ theta_2(s_)*n1*R0)/(DenL2);
+l10= 1-(1-l20)*theta_2(s_)/theta_1*R0;
 x0=-(c20-c10)*(psi*c20^(-sigma))-((l10/(1-l10))*R0-l20/(1-l20))*(1-psi)+btild_1*psi*c20^(-sigma);
 R0=c20^(-sigma)/c10^(-sigma);
 
 
 
 %-- Simulate the MODEL -------------------------------------------------
-NumSim=50000;
-rHist0 = rand(NumSim,1);
-K=1;
-ex(1).casename='b_{-1}=-1'; 
-Para.saveSimPath= ['~/Golosov-Sargent/Data/temp/SimDataGShocksLongSampleProductivity.mat'];
+NumSim=100;
 
-for ctrb=1:K
-  CoeffFileName='~/Golosov-Sargent/Data/temp/cproductivity.mat';
-SimData(ctrb)=RunSimulationsFromT1AltThetaShocks(CoeffFileName,x0,R0,NumSim,Para,rHist0);
+K=100;
+ex(1).casename='b_{-1}=-1'; 
+Para.saveSimPath= ['~/Golosov-Sargent/Data/temp/SimBootstrapProductivity.mat'];
+
+parfor ctrb=1:K
+    rHist0 = rand(NumSim,1);
+    CoeffFileName='~/Golosov-Sargent/Data/temp/cproductivity.mat';
+    SD(ctrb)=RunSimulationsFromT1AltThetaShocks(CoeffFileName,x0,R0,NumSim,Para,rHist0);
 end
+save(Para.saveSimPath,'SD');
 
 
