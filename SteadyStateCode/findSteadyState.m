23715d0a0c47f065c21ece2158d48629592db80f
David Evans <dgevans42@gmail.com>
2013-03-16 10:36:52 -0400

    changes to find steady state

--- SteadyStateCode/findSteadyState.m
+++ SteadyStateCode/findSteadyState.m
@@ -3,7 +3,6 @@ function [ x,R,PolicyRule ] = findSteadyState( x0,R0,Para)
 %   Detailed explanation goes here
       S = length(Para.P);
       if min(Para.theta_2) > 0
-        if length(Para.theta_2) == 1
             cRat = R0^(-1/Para.sigma);
             c1 = (0.8*(Para.n1*Para.theta_1+Para.n2*Para.theta_2)-Para.g)/(Para.n1+cRat*Para.n2);
             c2_ = cRat*c1; c2_(S) = [];
@@ -12,7 +11,7 @@ function [ x,R,PolicyRule ] = findSteadyState( x0,R0,Para)
             [xSS,~,~] = fsolve(@(x) SteadyStateResiduals(x,x0,R0,Para,1),[c1 c2_],options);
             [~, c1_, c2_, l1_, l2_] = SteadyStateResiduals(xSS,x0,R0,Para,1);
 
-            X0 = [c1_,c2_,l1_,l2_,R0,x0];
+            X0 = [c1_,c2_,l1_,l2_,x0,R0];
             nMult = 3*S+2;
 
             f = @(Mult) findMultipliers(X0,Mult,Para);
@@ -24,10 +23,6 @@ function [ x,R,PolicyRule ] = findSteadyState( x0,R0,Para)
             end
             Mult = A\b;
             X0(4*S+3:7*S+4) = Mult;
-        else
-            nMult = 3*S+2;
-            X0 = [0.5*ones(4*S,1);x0;R0;zeros(nMult,1)]';
-        end
       else
           nMult = 2*S+2;
           X0 = [0.5*ones(3*S,1);x0;R0;zeros(nMult,1)]';
@@ -49,6 +44,9 @@ function [ x,R,PolicyRule ] = findSteadyState( x0,R0,Para)
       
 end
 
+function [fvec, user, iflag] = SSResidualsNAG(n,x,user,iflag)
+    fvec = SSResiduals(x,user.Para);
+end
 
 function [res] = findMultipliers(X,Mult,Para)
     S = length(Para.P);
