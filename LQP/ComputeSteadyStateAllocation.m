33c004f4eec1f47a798a9ebb8d5ea99d485fbadf
anmol <anmol@anmol-ThinkPad-X1-Carbon.(none)>
2013-03-20 18:42:42 -0400

    compute lq matrices using sage

---
+++ LQP/ComputeSteadyStateAllocation.m
@@ -0,0 +1,20 @@
+
+function res=ComputeSteadyStateAllocation(x,ss_b2,ss_rho,Param)
+ss_theta_1=Param.ss_theta_1;
+ss_theta_2=Param.ss_theta_2;
+ss_g=Param.ss_g;
+sigma=Param.sigma;
+gamma=Param.gamma;
+beta=Param.beta;
+ss_c1=x(1);
+ss_c2=x(2);
+ss_l1=x(3);
+ss_l2=x(4);
+phi_1=ss_c1^(sigma)*ss_l1^(gamma);
+phi_2=ss_c2^(sigma)*ss_l2^(gamma);
+
+res(1)=ss_c1^sigma*ss_l1^(gamma + 1) - ss_c2^sigma*ss_l2^(gamma + 1) + beta*ss_b2 - ss_b2 - ss_c1 + ss_c2;
+res(2)=-ss_l1*ss_theta_1 - ss_l2*ss_theta_2+ ss_c1 + ss_c2 - ss_g;
+res(3)=phi_1*ss_theta_2 - phi_2*ss_theta_1;
+res(4)=ss_c2^(-sigma)/ss_c1^(-sigma)-ss_rho;
+end
