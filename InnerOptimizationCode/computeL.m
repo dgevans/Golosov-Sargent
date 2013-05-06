9e002dea8b45c44945eba43d8cc3c4053a0f3e49
anmol <apb296@nyu.edu>
2013-05-06 14:46:21 +0900

    theta_1=0

--- InnerOptimizationCode/computeL.m
+++ InnerOptimizationCode/computeL.m
@@ -20,15 +20,21 @@ function [l1 gradl1 l2 gradl2] = computeL(c1,gradc1,c2,gradc2,Rprime,gradRprime,
         theta_2 = theta_2(:)';
         theta_2 = kron(ones(2*S-1,1),theta_2);
     end
+       
     %Compute l2 first
     l2 = (n1*c1+n2*c2+g+n1.*theta_2.*Rprime-n1.*theta_1  )./(theta_2.*(n2+Rprime*n1));
     %now gradl2
     gradl2 = n1*gradRprime./(n2+n1*Rprime) - n1*gradRprime.*l2./(n2+n1*Rprime)...
              +n1*gradc1./(theta_2.*(n2+n1*Rprime)) +n2*gradc2./(theta_2.*(n2+n1*Rprime));
+    
     %now l1
+    if min(theta_1)>0
     l1 = 1 - (1-l2).*Rprime.*theta_2./theta_1;
     gradl1 = gradl2.*Rprime.*theta_2./theta_1 - (1-l2).*gradRprime.*theta_2./theta_1;
-         
+    else
+      l1=0*g;
+      gradl1=0*g;
+    end
          
 end
 
